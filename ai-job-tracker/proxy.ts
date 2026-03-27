ea20766e2aadadebd46c459778f2b709665d022a
Theju <thesigany@gmail.com>
2026-03-27 23:59:46 +0530

    P1: proxy.ts change

--- ai-job-tracker/proxy.ts
+++ ai-job-tracker/proxy.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { getSessionCookie } from 'better-auth/cookies'
 
 export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl
   if (!pathname.startsWith('/dashboard')) return NextResponse.next()
 
-  const session = req.cookies.get('better-auth.session_token')
+  // const session = req.cookies.get('better-auth.session_token')
+  const session = getSessionCookie(req)
+
   if (!session) {
-    return NextResponse.redirect(new URL('/auth/signin', req.url))
+    const redirectUrl = new URL('/auth/signin', req.url)
+    redirectUrl.searchParams.set('callbackURL', req.nextUrl.pathname)
+    return NextResponse.redirect(redirectUrl)
   }
 
   return NextResponse.next()
 }
 
 export const config = { matcher: ['/dashboard/:path*'] }
+// __Secure-better-auth.session_token
