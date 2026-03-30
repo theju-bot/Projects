bff829847299b722444fca88cb5f6d8a91538866
Theju <thesigany@gmail.com>
2026-03-30 09:36:32 +0530

    P1: UI change

--- ai-job-tracker/app/dashboard/layout.tsx
+++ ai-job-tracker/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
-import { AIKeyBanner } from "../components/AIKeyBanner"
-import Navbar from '@/app/components/Navbar'
+import { AIKeyBanner } from '../components/AIKeyBanner'
+import { Sidebar } from '../components/Sidebar'
 
 export default function DashboardLayout({
   children,
@@ -8,12 +8,12 @@ export default function DashboardLayout({
 }) {
   return (
     <div className='flex h-screen overflow-hidden'>
+      <Sidebar />
       <div className='flex flex-col flex-1 overflow-hidden'>
         <div className='px-6 pt-4'>
           <AIKeyBanner />
         </div>
-        <Navbar />
-        <main className='flex-1 overflow-auto px-6 pb-6'>{children}</main>
+        <main className='flex-1 overflow-auto px-6 pb-20 md:pb-6'>{children}</main>
       </div>
     </div>
   )
