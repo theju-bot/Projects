56acbad451d8eb598d3989b55e906f04ca0a4e1c
theju <thesigany@gmail.com>
2026-04-24 10:12:53 +0530

    Changes

--- workout-tracker/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function POST(req: NextRequest) {
-  const response = NextResponse.redirect(
-    new URL('/auth/signin', process.env.NEXT_PUBLIC_BASE_URL),
-  )
-  response.cookies.delete('jwt')
-  return response
-}
--- workout-tracker/lib/error/AppError.ts
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  constructor(
-    public message: string,
-    public status: number,
-  ) {
-    super(message)
-    this.name = 'AppError'
-  }
-}
