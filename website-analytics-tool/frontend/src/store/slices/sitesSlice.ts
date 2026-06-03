a7ca169aaea9fa47e577cd82a894e94319e51ce7
theju <thesigany@gmail.com>
2026-06-03 23:54:49 +0530

    removed siteSlices

--- website-analytics-tool/frontend/src/store/slices/sitesSlice.ts
+++ website-analytics-tool/frontend/src/store/slices/sitesSlice.ts
@@ -16,24 +16,13 @@ const sitesSlice = createSlice({
   name: 'sites',
   initialState,
   reducers: {
-    setSites: (state, action: PayloadAction<Site[]>) => {
-      state.sites = action.payload
-    },
-    addSite: (state, action: PayloadAction<Site>) => {
-      state.sites.push(action.payload)
-    },
-    removeSite: (state, action: PayloadAction<string>) => {
-      state.sites = state.sites.filter((s) => s._id !== action.payload)
-    },
     setSelectedSite: (state, action: PayloadAction<Site>) => {
       state.selectedSite = action.payload
     },
   },
 })
 
-export const { setSites, addSite, removeSite, setSelectedSite } =
-  sitesSlice.actions
+export const { setSelectedSite } = sitesSlice.actions
 export default sitesSlice.reducer
 
-export const selectSites = (state: RootState) => state.sites.sites
 export const selectSelectedSite = (state: RootState) => state.sites.selectedSite
