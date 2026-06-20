24c958891f7a0bea156be5caec8eb13dfbb217ce
theju <thesigany@gmail.com>
2026-06-20 18:36:32 +0530

    feat: enhance meta tags in index.html for improved SEO and social sharing

--- real-time-collaborative-notes/frontend/src/lib/socket.ts
+++ real-time-collaborative-notes/frontend/src/lib/socket.ts
@@ -1,8 +1,11 @@
 import { io } from 'socket.io-client'
 
+const isDev = import.meta.env.MODE === 'development'
+
 const socket = io(import.meta.env.VITE_SERVER_URL, {
-    withCredentials: true,
-    autoConnect: false
+  path: isDev ? '/socket.io' : '/noteflow/socket.io',
+  withCredentials: true,
+  autoConnect: false,
 })
 
 export default socket
